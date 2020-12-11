14e1e6a9a173e55338f44ac332aed437bd67ea23
Hammertonmutuku <lthamma3@gmail.com>
2020-12-11 17:29:53 +0300

    first commit

---
+++ includes/header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title><?php echo $title ?></title>
+    <link rel="icon" type="image/jpg" href="images/h-logo1.png">
+    <link rel = "stylesheet" type= "text/css" href="./css/styles.css">  
+    <link rel="stylesheet" href="css/bootstrap-4.3.1-dist/css/bootstrap.min.css">
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+</head>
+<body>
+    <div class = "container">
+        <!-- navigation bar -->
+        <nav class="navbar navbar-expand-sm navbar-dark bg-transparent ">
+            <a class="navbar-brand" href="index.php"><img class="img-fluid logo" src="./images/h-logo1.png"
+                    alt="logo" height= "86px" width = "46px"> HammertonMutuku</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse"
+                data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav ml-auto mynav">
+                    <a class="nav-item nav-link" href="#about">About </a>
+                    <a class="nav-item nav-link" href="#projects">My work</a>
+                    <a class="nav-item nav-link" href="#contact">Contact</a>
+
+                </div>
+            </div>
+        </nav>
